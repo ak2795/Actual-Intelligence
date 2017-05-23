19b37b9a4d83f7d60199f73b41546e76b0795d48
Jonathan <jonatan1626@gmail.com>
2017-05-22 23:38:16 -0700

    init

--- decisionTree.py
+++ decisionTree.py
@@ -1,5 +1,9 @@
 from sklearn import tree
 import numpy
+from pprint import pprint
 
 def decisionTree():
-    print ("HI THERE")
+    with open(features_labels.json) as data_file:
+        data = json.load(data_file)
+
+    pprint(data)
