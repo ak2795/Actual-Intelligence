c168b8c6099abd067b7211967d7bd27e8f1c6685
KrazyKoala <rafffung@gmail.com>
2017-05-22 17:35:24 -0700

    Modified parser to include labels and also export features and labels to JSON

--- marsys_parser.py
+++ marsys_parser.py
@@ -1,34 +1,65 @@
 import sys
+import json
 
 def parseVars(fstream):
-    tempDict = {}
+    attrArr = []
     count = 0
     tempArr = []
+    labels = []
     for line in fstream:
         split = line.split(" ")
         if (count % 3 == 0):
             fileNameSplit = split[2].split("/")
-            fName = fileNameSplit[len(fileNameSplit)-1]
+            split_len = len(fileNameSplit)
+            fName = fileNameSplit[split_len-1]
+            genre = fileNameSplit[split_len-2]
+            if genre == "blues":
+                labels.append(0)
+            elif genre == "classical":
+                labels.append(1)
+            elif genre == "country":
+                labels.append(2)
+            elif genre == "disco":
+                labels.append(3)
+            elif genre == "hiphop":
+                labels.append(4)
+            elif genre == "jazz":
+                labels.append(5)
+            elif genre == "metal":
+                labels.append(6)
+            elif genre == "pop":
+                labels.append(7)
+            elif genre == "reggae":
+                labels.append(8)
+            elif genre == "rock":
+                labels.append(9)
+            else:
+                print("AHHHHHH")
+
+
         elif (count % 3 == 1):
             srate = split[2]
         else:
             values = line.split(",")
-            for i in values:
-                if (i != "music"):
-                    tempArr.append(i)
-            tempDict[fName] = tempArr
-            tempArr = []
-            fileName = []
+            # for i in values:
+            #     if (i != "music"):
+            #         tempArr.append(i)
+            # print tempArr
+            del values[-1]
+            values = map(float, values)
+            attrArr.append(values)
 
         count += 1
-    print (count)
-    return tempDict
+    # print (count)
+    # print ("\nlabels:\n{}\n".format(labels))
+    # print attrArr[0:3]
+    return (attrArr, labels)
 
 def scanData(tempInput, attrList):
     # fname = input("Enter Filename: ")
     marsysFile = open(tempInput, "r")
     print ("Opened: " + tempInput)
-    tempAttr = {}
+    feat_labels = ()
 
     for line in marsysFile:
         split = line.split(" ")
@@ -44,20 +75,23 @@ def scanData(tempInput, attrList):
         # detects that the rest is data and parses it and breaks out of loop
         elif (split[0] == "@data\n"):
             print ("HERE")
-            tempAttr = parseVars(marsysFile)
+            feat_labels = parseVars(marsysFile)
             break
 
     marsysFile.close()
-    return tempAttr
+    return feat_labels
 
 def main():
     fname = "MARSYAS_SINGLEfeatures.arff"
     attrNameList = []
-    attrNameToValues = scanData(fname, attrNameList)
+    feat_labels = scanData(fname, attrNameList)
 
-    print (attrNameList)
-    for key, value in attrNameToValues.items() :
-        print(key)
+    print feat_labels[1]
+    with open("features_labels.json", 'w') as outfile:
+        json.dump(feat_labels, outfile, indent=3)
+    # print (attrNameList)
+    # for key, value in attrNameToValues.items():
+    #     print(value)
 
 
 if __name__ == "__main__":
