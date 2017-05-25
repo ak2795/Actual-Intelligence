2c38127c97ee49ba26da071bc3cfe922b864934d
jonatan1626 <jonatan1626@gmail.com>
2017-05-25 00:24:51 -0700

    Decision Trees Done

--- decisionTree.py
+++ decisionTree.py
@@ -1,18 +1,54 @@
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 import json
 import numpy
 from pprint import pprint
 
 def decisionTree():
-    with open('features_labels.json') as data_file:
-        data = json.load(data_file)
+    for numTimes in range(10):
 
-    # for i in range(len(data)):
-    #     for j in range(len(data[i])):
-    #         print(data[i][j])
+        print("Using: tests/features_labelsTrain" + str(numTimes) + ".json")
+        with open('tests/features_labelsTrain' + str(numTimes) + '.json') as data_file:
+            data = json.load(data_file)
+
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(data[0], data[1])
+
+        with open('tests/features_labelsTest' + str(numTimes) + '.json') as data_file:
+            dataTest = json.load(data_file)
+
+        count = 0
+        total = 0
+        tempPred = clf.predict(dataTest[0])
+        answers = dataTest[1]
+
+        for i in range(len(answers)):
+            if (tempPred[i] == answers[i]):
+                count += 1
+            total += 1
+
+        print("\tDecision Tree: " + str(count) + " out of " + str(total))
+
+        rtf = RandomForestClassifier(n_estimators=100)
+        rtf = rtf.fit(data[0], data[1])
+        tempPred = rtf.predict(dataTest[0])
+
+        count = 0
+        total = 0
+        for i in range(len(answers)):
+            if (tempPred[i] == answers[i]):
+                count += 1
+            total += 1
+
+        print("\tRandom Forests: " + str(count) + " out of " + str(total))
 
 
 
+def main():
+    decisionTree()
+
+if __name__ == "__main__":
+    main()
 
 # 3jgqhxg
 # http://whenisgood.net/3d39dyh/results/3jgqhxg
