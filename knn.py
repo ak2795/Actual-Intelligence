fa9a7d91717dbc5ead03edd111116ed9cc232d23
Andrew Kim <andrew.kym7@gmail.com>
2017-06-04 20:03:02 -0700

    more on display code

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ from sklearn import manifold
 import numpy as np
 import json
 from pprint import pprint
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
 def kneighbors():
@@ -22,17 +23,57 @@ def printKNN(data_x, data_y):
     X = []
     Y = data_y
 
+    n_neighbors = 30
+
     # get the first two feature for all training data
     for row in data_x:
         X.append(row[:2])
 
     h = .02 #step size in the mesh
 
-    knn = KNeighborsClassifier()
-    knn.fit(X, Y) # fit the data
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
-    # Plot the decision boundary
-    x_min, x_max = X[:,0].min() - 0.5, X[:,0].max() + 0.5
+    for weights in ['uniform', 'distance']:
+
+        knn = KNeighborsClassifier(n_neighbors, weights=weights)
+        knn.fit(X, Y) # fit the data
+        
+        x_min, x_max = X[0][0], X[0][0]
+        y_min, y_max = X[0][0], X[0][0]
+
+        # Plot the decision boundary
+        for row in X:
+            if row[0] > x_max:
+                x_max = row[0]
+            if row[1] > y_max:
+                y_max = row[1]
+            if row[0] < x_min:
+                x_min = row[0]
+            if row[1] < y_min:
+                y_min = row[1]
+
+        # print("x_max: " + str(x_max))
+        # print("x_min: " + str(x_min))
+        # print("y_max: " + str(y_max))
+        # print("y_min: " + str(y_min))
+
+        x_max += .5
+        x_min -= .5
+        y_max += .5
+        y_min -= .5
+
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                            np.arange(y_min, y_max, h))
+        Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.figure()
+        plt.pcolormesh(xx, yy ,Z, cmap=cmap_light)
+
+plt.show()
+                 
 
 def testKNN():
     for numTimes in range(10):
