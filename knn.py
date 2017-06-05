4a90b292c318b2b5586188f4b43a9209d6b6ab1f
Andrew Kim <andrew.kym7@gmail.com>
2017-06-04 18:17:10 -0700

    display

--- knn.py
+++ knn.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 
 def kneighbors():
-    for numTimes in range(10):
+    for numTimes in range(1):
         print("Using: tests/features_labelsTrain" + str(numTimes) + ".json")
         with open('tests/features_labelsTrain' + str(numTimes) + '.json') as data_file:
             data = json.load(data_file)
@@ -14,8 +14,25 @@ def kneighbors():
         # using k = 30
         knn = KNeighborsClassifier(n_neighbors = 30)
         neigh = knn.fit(data[0], data[1])
+        printKNN(data[0], data[1])
 
         return neigh
+
+def printKNN(data_x, data_y):
+    X = []
+    Y = data_y
+
+    # get the first two feature for all training data
+    for row in data_x:
+        X.append(row[:2])
+
+    h = .02 #step size in the mesh
+
+    knn = KNeighborsClassifier()
+    knn.fit(X, Y) # fit the data
+
+    # Plot the decision boundary
+    x_min, x_max = X[:,0].min() - 0.5, X[:,0].max() + 0.5
 
 def testKNN():
     for numTimes in range(10):
@@ -36,7 +53,8 @@ def testKNN():
         print("\tk-Nearest Neighbors: " + str(count) + " out of " + str(total))
 
 def main():
-    testKNN()
+    # testKNN()
+    kneighbors()
 
 if __name__ == "__main__":
     main()
